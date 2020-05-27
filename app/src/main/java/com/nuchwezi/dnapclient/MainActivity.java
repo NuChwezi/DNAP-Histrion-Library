b423a042fbd19f7fd924e47c34a96aa131821501
Ow. Nemesis Fixx <joewillrich@gmail.com>
2020-05-27 14:12:06 +0300

    Use standard DNAP nomenclature and fix issues in running Histrion UI

--- app/src/main/java/com/nuchwezi/dnapclient/MainActivity.java
+++ app/src/main/java/com/nuchwezi/dnapclient/MainActivity.java
@@ -2,7 +2,10 @@ package com.nuchwezi.dnapclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void switchToDNAP_Persona(String personaUUID, String autoInstallChannel) {
+        Intent _intent = new Intent(MainActivity.this, com.nuchwezi.dnaphistrion.HistrionMainActivity.class);
+
+        if(personaUUID != null){
+            _intent.putExtra("PERSONA:UUID", personaUUID);
+        }
+
+        if(autoInstallChannel != null){
+            _intent.putExtra("CHANNEL:NAME", autoInstallChannel);
+        }
+
+        startActivity(_intent);
+    }
+
+    public void launchDNAPPersona(View view) {
+        try {
+            switchToDNAP_Persona("bb765c31-6959-49d0-b192-6c83bdab5cb4", "NUCHWEZI2");
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.e("DNAP:Client", e.getMessage());
+        }
     }
 }
