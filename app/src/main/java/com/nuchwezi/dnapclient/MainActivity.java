6468d50ccb28f648c390ce86c2b50f65ef63f3f6
Ow. Nemesis Fixx <joewillrich@gmail.com>
2020-05-27 11:17:20 +0300

    start app to demonstrate using embedded Histrion via DNAP library

---
+++ app/src/main/java/com/nuchwezi/dnapclient/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nuchwezi.dnapclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
